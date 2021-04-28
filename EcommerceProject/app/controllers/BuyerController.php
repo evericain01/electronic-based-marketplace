be69f0711e8cee3cdbe06d1a46d4d1929d869607
Christian <christianpauld002@gmail.com>
2021-04-28 16:37:15 -0400

    forgot to pull

--- EcommerceProject/app/controllers/BuyerController.php
+++ EcommerceProject/app/controllers/BuyerController.php
@@ -23,7 +23,6 @@ class BuyerController extends \App\core\Controller {
             $buyer = $buyer->findUserId($_SESSION['user_id']);
 
             $this->view('Buyer/buyerMainPage', ['products' => $products, 'buyer' => $buyer]);
-            
         }
     }
 
