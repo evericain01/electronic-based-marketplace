2f5a4fd45d6d2e71e84bf7f4caa9b56aefc72805
Christian <christianpauld002@gmail.com>
2021-05-04 15:44:45 -0400

    forgot to prepare invoices view

--- EcommerceProject/app/views/Invoice/listAllOrders.php
+++ EcommerceProject/app/views/Invoice/listAllOrders.php
@@ -1,15 +1,16 @@
 <html>
     <head>
-        <title>Orders</title>
+
+        <title><?= _("Orders")?></title>
     </head>
     <body>
-        <h2>All Orders:</h2>
+        <h2><?= _("All Orders")?>:</h2>
         <?php
-        echo "<br><a href='" . BASE . "/Buyer/index'>&#8592 Go Back to Main Page</a><br><br><br>";
+        echo "<br><a href='" . BASE . "/Buyer/index'>&#8592 ". _("Go Back to Main Page")."</a><br><br><br>";
 
 
         if (empty($data["invoice"])) {
-            echo "<i>You have no orders.</i>";
+            echo "<i>". _("You have no orders").".</i>";
         }
         
         
@@ -17,23 +18,23 @@
         foreach ($data["invoice"] as $invoice) {
             foreach ($data["products"] as $product) {
                 if ($invoice->product_id == $product->product_id) {
-                    echo "<b><u>Item Name:</u></b> $product->caption<br>";
-                    echo "<b><u>Item Description:</u></b> $product->description<br>";
+                    echo "<b><u>". _("Item Name").":</u></b> $product->caption<br>";
+                    echo "<b><u>". _("Item Description").":</u></b> $product->description<br>";
                     $quantity = $invoice->total / $product->price;
-                    echo "<b><u>Quantity: </u>$quantity<b><br>";
-                    echo "<b><u>Cost Per Item:</u></b> $product->price CAD<br>";
-                    echo "<b><u>Total:</u></b> $invoice->total CAD<br>";
+                    echo "<b><u>". _("Quantity").": </u>$quantity<b><br>";
+                    echo "<b><u>". _("Cost Per Item").":</u></b> $product->price CAD<br>";
+                    echo "<b><u>". _("Total").":</u></b> $invoice->total CAD<br>";
                     foreach ($data["sellers"] as $seller) {
                         if ($invoice->seller_id == $product->seller_id) {
-                            echo "<b><u>Seller Name:</u> </b>$seller->first_name ";
+                            echo "<b><u>". _("Seller Name").":</u> </b>$seller->first_name ";
                             echo "$seller->last_name<br>";
-                            echo "<b><u>Seller Company:</u> </b>$seller->brand_name<br><br>";
+                            echo "<b><u>". _("Seller Company").":</u> </b>$seller->brand_name<br><br>";
                         }
                     }
                 }
             }
-            echo "DATE PURCHASED: $invoice->timestamp<br>";
-            echo "EXPECTED DELIVERY DATE: $invoice->date_of_arrival<br><br>";
+            echo "". _("DATE PURCHASED").": $invoice->timestamp<br>";
+            echo "". _("EXPECTED DELIVERY DATE").": $invoice->date_of_arrival<br><br>";
 
             if ($invoice->status == "Delivered") {
                 $exist = false;
@@ -47,15 +48,15 @@
                     }
                 }
 
-                echo "STATUS: <span style='color:#398C0C;text-align:center;'> Delivered </span> &mdash; $invoice->date_of_arrival";
+                echo _("STATUS").": <span style='color:#398C0C;text-align:center;'> Delivered </span> &mdash; $invoice->date_of_arrival";
                 if ($exist == true) {
-                    echo "<br><a href='" . BASE . "/Review/edit/$currentProductReviewID'>Edit Review</a><br><br><br>";
+                    echo "<br><a href='" . BASE . "/Review/edit/$currentProductReviewID'>". _("Edit Review")."</a><br><br><br>";
                 } else {
-                    echo "<br><a href='" . BASE . "/Review/add/$invoice->product_id'>Leave a Review?</a><br><br><br>";
+                    echo "<br><a href='" . BASE . "/Review/add/$invoice->product_id'>". _("Leave a Review")."?</a><br><br><br>";
                 }
             } else {
-                echo "STATUS: <span style='color:#C77800;text-align:center;'> In Transit </span>";
-                echo "<a href='" . BASE . "/Invoice/updateStatus/$invoice->invoice_id'>Recieved?</a><br>";
+                echo _("STATUS").": <span style='color:#C77800;text-align:center;'> ". _("In Transit")." </span>";
+                echo "<a href='" . BASE . "/Invoice/updateStatus/$invoice->invoice_id'>". _("Recieved")."?</a><br>";
             }
             echo "<hr style='width:325px;text-align:left;margin-left:0'><br>";
         }
--- EcommerceProject/app/views/Invoice/listInvoice.php
+++ EcommerceProject/app/views/Invoice/listInvoice.php
@@ -1,12 +1,12 @@
 <html>
     <head>
-        <title>Invoice</title>
+        <title><?= _("Invoice")?></title>
     </head>
     <body>
-        <h2>Invoice:</h2>
+        <h2><?= _("Invoice")?>:</h2>
 
         <?php
-        echo "<br><a href='" . BASE . "/Buyer/index'>&#8592 Go Back to Main Page</a><br><br><br>";
+        echo "<br><a href='" . BASE . "/Buyer/index'>&#8592 ". _("Go Back to Main Page")."</a><br><br><br>";
 
         $date = new DateTime(null, new DateTimeZone("America/Toronto"));
         $result = $date->format('Y-m-d H:i:s');
@@ -15,22 +15,22 @@
             foreach ($data["products"] as $product) {
                 if ($invoice->product_id == $product->product_id && $invoice->timestamp == $result) {
                    
-                    echo "<b><u>Item Name:</u></b> $product->caption<br>";
-                    echo "<b><u>Item Description:</u></b> $product->description<br>";
+                    echo "<b><u>". _("Item Name").":</u></b> $product->caption<br>";
+                    echo "<b><u>". _("Item Description").":</u></b> $product->description<br>";
                     $quantity = $invoice->total / $product->price;
-                    echo "<b><u>Quantity: </u>$quantity<b><br>";
-                    echo "<b><u>Cost Per Item:</u></b> $product->price CAD<br>";
-                    echo "<b><u>Total:</u></b> $invoice->total CAD<br>";
+                    echo "<b><u>". _("Quantity").": </u>$quantity<b><br>";
+                    echo "<b><u>". _("Cost Per Item").":</u></b> $product->price CAD<br>";
+                    echo "<b><u>". _("Total").":</u></b> $invoice->total CAD<br>";
                     foreach ($data["sellers"] as $seller) {
                         if ($invoice->seller_id == $product->seller_id) {
-                            echo "<b><u>Seller Name:</u> </b>$seller->first_name ";
+                            echo "<b><u>". _("Seller Name").":</u> </b>$seller->first_name ";
                             echo "$seller->last_name<br>";
-                            echo "<b><u>Seller Company:</u> </b>$seller->brand_name<br><br>";
+                            echo "<b><u>". _("Seller Company").":</u> </b>$seller->brand_name<br><br>";
                         }
                     }
-                    echo "DATE PURCHASED: $invoice->timestamp<br>";
+                    echo _("DATE PURCHASED").": $invoice->timestamp<br>";
 
-                    echo "EXPECTED DELIVERY DATE: $invoice->date_of_arrival<br><br>";
+                    echo _("EXPECTED DELIVERY DATE").": $invoice->date_of_arrival<br><br>";
                     echo "<hr style='width:325px;text-align:left;margin-left:0'><br>";
                 }
             }
