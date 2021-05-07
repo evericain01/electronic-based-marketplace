b70e4440a346b130c6758554ba0461b4f8f2a33e
evericain01 <evericain01@hotmail.com>
2021-05-06 23:37:54 -0400

    updated database file

--- EcommerceProject/marketplace_db.sql
+++ EcommerceProject/marketplace_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2021 at 05:59 PM
+-- Generation Time: May 07, 2021 at 03:22 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -35,7 +35,7 @@ CREATE TABLE `buyer` (
   `user_id` int(11) NOT NULL,
   `first_name` varchar(64) NOT NULL,
   `last_name` varchar(64) NOT NULL,
-  `budget` double(5,2) NOT NULL
+  `budget` double(9,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `buyer` (
 --
 
 INSERT INTO `buyer` (`buyer_id`, `user_id`, `first_name`, `last_name`, `budget`) VALUES
-(1, 2, 'Ebra', 'Veri', 100.67);
+(6, 47, 'Jon', 'White', 89225.00);
 
 -- --------------------------------------------------------
 
@@ -54,7 +54,8 @@ INSERT INTO `buyer` (`buyer_id`, `user_id`, `first_name`, `last_name`, `budget`)
 DROP TABLE IF EXISTS `cart`;
 CREATE TABLE `cart` (
   `product_id` int(11) NOT NULL,
-  `buyer_id` int(11) NOT NULL
+  `buyer_id` int(11) NOT NULL,
+  `product_quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -70,8 +71,19 @@ CREATE TABLE `invoice` (
   `buyer_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `date_of_arrival` int(2) NOT NULL
+  `date_of_arrival` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total` double(10,2) NOT NULL,
+  `status` enum('Delivered','In Transit') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `invoice`
+--
+
+INSERT INTO `invoice` (`invoice_id`, `seller_id`, `buyer_id`, `product_id`, `timestamp`, `date_of_arrival`, `total`, `status`) VALUES
+(163, 6, 6, 12, '2021-05-06 17:25:17', '2021-05-13 04:00:00', 1040.00, 'In Transit'),
+(164, 6, 6, 13, '2021-05-06 17:27:20', '2021-05-13 04:00:00', 40.00, 'In Transit'),
+(165, 6, 6, 11, '2021-05-06 17:37:08', '2021-05-13 04:00:00', 1400.00, 'Delivered');
 
 -- --------------------------------------------------------
 
@@ -90,6 +102,22 @@ CREATE TABLE `product` (
   `price` double(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `seller_id`, `filename`, `caption`, `description`, `quantity`, `price`) VALUES
+(11, 6, '6092bb3913f37.png', 'i9 9700k', 'Coffee Lake 8-Core 3.6 GHz (4.9 GHz Turbo) LGA 1151', 5, 700.00),
+(12, 6, '6092bb8f50d19.png', 'Sennheiser Headphones', 'HD 450BT. 18 Hz - 22,000 Hz; Bluetooth® 5.0.', 0, 260.00),
+(13, 6, '6092bd52523e9.png', 'Gaming Mouse', 'Natec Genesis G66 NMG-0662', 20, 20.00),
+(14, 6, '6092bdc0bb85f.png', 'RT-AC68U', 'AC1900 Dual Band Gigabit WiFi Router', 24, 250.00),
+(15, 6, '6092be4d54a35.png', 'USB Key', 'Kingston 32GB DataTraveler Kyson', 92, 15.00),
+(16, 6, '6092bf3d9293d.png', 'Samsung TV', '55\" 2020 TU8000 Smart 4K UHD TV', 14, 1100.00),
+(23, 8, '60936cc78ca5f.png', 'Samsung Galaxy S20', 'FE 5G SM-G781U 128GB Smartphone (Unlocked)\r\n', 37, 1150.00),
+(24, 8, '60936ce791b0d.png', 'Canon Camera', 'Canon EOS Rebel T7i EF-S 18-55mm IS STM Lens Kit', 15, 800.00),
+(25, 8, '60936d097a485.png', 'SD Card', 'SanDisk 128GB Extreme PRO SDXC', 50, 40.00),
+(26, 8, '60936d2aefef2.png', 'Apple Watch', 'Series 6 Gold Aluminium Case with Sport Band', 17, 700.00);
+
 -- --------------------------------------------------------
 
 --
@@ -101,9 +129,16 @@ CREATE TABLE `review` (
   `review_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `buyer_id` int(11) NOT NULL,
-  `rate` enum('1','2','3','4','5') NOT NULL,
+  `rate` enum('1/5','2/5','3/5','4/5','5/5') NOT NULL,
   `text_review` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `review`
+--
+
+INSERT INTO `review` (`review_id`, `product_id`, `buyer_id`, `rate`, `text_review`) VALUES
+(20, 11, 6, '5/5', 'Amazing product. A++');
 
 -- --------------------------------------------------------
 
@@ -125,7 +160,8 @@ CREATE TABLE `seller` (
 --
 
 INSERT INTO `seller` (`seller_id`, `user_id`, `first_name`, `last_name`, `brand_name`) VALUES
-(1, 3, 'sellerName', 'sellerLast', 'TEchno');
+(6, 32, 'Timmy ', 'Matthews', 'TechoBiz'),
+(8, 49, 'Anthony', 'Brown', 'Electric.co');
 
 -- --------------------------------------------------------
 
@@ -138,17 +174,17 @@ CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `username` varchar(64) NOT NULL,
   `password_hash` varchar(225) NOT NULL,
-  `secret_key` varchar(16) DEFAULT NULL
+  `user_role` enum('seller','buyer') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `password_hash`, `secret_key`) VALUES
-(1, 'tarzan123', '$2y$10$p2Gx4R4IR76vxdio0V/4leKZvn8QlXTHvW9Q4H7wxh9/eK1eRHPfW', NULL),
-(2, 'tarzan123', '$2y$10$AL04DIw.rLI0wckHPBdROeWNyYrwk5MZ948pHdyLeIxEnPcMwRpn2', NULL),
-(3, 'sellertest123', '$2y$10$PoOkKC9iulV7H/pS0DxTm.M5IztxdnYGYywr2dGMAIA3AArh2Ru1O', NULL);
+INSERT INTO `user` (`user_id`, `username`, `password_hash`, `user_role`) VALUES
+(32, 'seller123', '$2y$10$aRqNv9EcVM0GywLNYa/WIuc5Ulp/zFAsse0T4zndu5VzwcXSkiFbS', 'seller'),
+(47, 'buyer123', '$2y$10$CFOBBS1MDET8416x0/vHuuEA2YyLy6oi3iWr6VnTtndIYxV6EeKaK', 'buyer'),
+(49, 'seller1234', '$2y$10$hJbEmT84CeW3XKNPDh4V..58dP7XcsZozPhYMQttLLu6ZAg/WmToy', 'seller');
 
 --
 -- Indexes for dumped tables
@@ -213,37 +249,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `buyer`
 --
 ALTER TABLE `buyer`
-  MODIFY `buyer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `buyer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
-  MODIFY `invoice_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `invoice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=166;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `seller`
 --
 ALTER TABLE `seller`
-  MODIFY `seller_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `seller_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
